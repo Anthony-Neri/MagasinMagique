c6f820a84d6dec2ac8ec87756b899dfc31976ccd
Anthony <242581876+Anthony-Neri@users.noreply.github.com>
2026-03-25 11:07:35 +0100

    Ajout du test pour le Comté

--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -13,5 +13,27 @@ class MagasinTest {
         app.updateQuality();
         assertEquals("fixme", app.items[0].name);
     }
+    @Test
+    void checkUpdateQualityComte(){
+        Item[] items = new Item[] { new Item("Comté", 10, 10) };
+        Item[] itemsV2 = new Item[] { new Item("Comté", 10, 10) };
+
+        Magasin app = new Magasin(items);
+        MagasinV2 appV2 = new MagasinV2(itemsV2);
+        for (int i = 0 ; i < 50 ; i++){
+
+            app.updateQuality();
+            appV2.updateQuality();
+
+            assertEquals(app.items[0].quality,appV2.items[0].quality);
+            assertEquals(app.items[0].sellIn,appV2.items[0].sellIn);
+
+        }
+
+
+
+    }
 
 }
+
+
