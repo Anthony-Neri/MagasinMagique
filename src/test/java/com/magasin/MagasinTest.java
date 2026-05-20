bdb766146ff941e7b960317fd8d6d5b333d85afa
Anthony <242581876+Anthony-Neri@users.noreply.github.com>
2026-05-20 15:24:51 +0200

    Update MagasinTest.java

--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -7,14 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MagasinTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[]{new Item("foo", 0, 0)};
-        Magasin app = new Magasin(items);
-        app.updateQuality();
-        assertEquals("foo", app.items[0].name);
-    }
-
-    @Test
     void checkUpdateQualityComte() {
         Item[] items = new Item[]{
                 new Item("Comté", 10, 50),
